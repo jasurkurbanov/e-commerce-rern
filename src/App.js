67b4d51af03b60e0328cc2bdd40e2a0b1dfdc6ba
Jasur Kurbanov <jasurkurbanov96@gmail.com>
2022-08-04 18:34:51 +0500

    feat: home page started

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styles from "./style.module.css";
+import clsx from "clsx";
+import { BsSearch } from "react-icons/bs";
+import rand from "./assets/rand.png";
 
 function App() {
+  const [sidebarExpanded, setSidebarExpanded] = useState(false);
+
+  const onToggleSidebar = () => setSidebarExpanded(!sidebarExpanded);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div
+        className={clsx(
+          styles.leftSidebarDefault,
+          sidebarExpanded && styles.leftSidebarExpanded
+        )}
+      >
+        <button onClick={onToggleSidebar}>Open</button>1
+      </div>
+
+      <div
+        className={clsx(styles.main, sidebarExpanded && styles.mainExpanded)}
+      >
+        <div className={styles.content}>
+          <div className={styles.searchContainer}>
+            <p className={styles.searchTitle}>Search Item</p>
+            <div className={styles.searchInput}>
+              <input placeholder="Apple Watch, Samsung S21, Macbook Pro, ..." />
+              <BsSearch />
+            </div>
+          </div>
+          <div class={styles.gridContainer}>
+            <div class={styles.gridItem}>
+              <div className={styles.gridImage}>
+                <img src={rand} />
+              </div>
+              <div className={styles.productInfo}>
+                <p className={styles.productTitle}>Apple Watch</p>
+                <span className={styles.productType}>Series 5 SE</span>
+                <div>
+                  <span className={styles.productPrice}>$ 529.99</span>
+                </div>
+              </div>
+            </div>
+            <div class={styles.gridItem}>
+              <div className={styles.gridImage}>
+                <img src={rand} />
+              </div>
+            </div>
+            <div class={styles.gridItem}>
+              <div className={styles.gridImage}>
+                <img src={rand} />
+              </div>
+            </div>
+            <div class={styles.gridItem}>
+              <div className={styles.gridImage}>
+                <img src={rand} />
+              </div>
+            </div>
+            <div class={styles.gridItem}>
+              <div className={styles.gridImage}>
+                <img src={rand} />
+              </div>
+            </div>
+            <div class={styles.gridItem}>
+              <div className={styles.gridImage}>
+                <img src={rand} />
+              </div>
+            </div>
+            <div class={styles.gridItem}>
+              <div className={styles.gridImage}>
+                <img src={rand} />
+              </div>
+            </div>
+            <div class={styles.gridItem}>
+              <div className={styles.gridImage}>
+                <img src={rand} />
+              </div>
+            </div>
+            <div class={styles.gridItem}>
+              <div className={styles.gridImage}>
+                <img src={rand} />
+              </div>
+            </div>
+            <div class={styles.gridItem}>
+              <div className={styles.gridImage}>
+                <img src={rand} />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.rightSidebar}>bag</div>
+      </div>
+    </>
   );
 }
 
